722237083234260f334d57fb0a180ca53bd91029
jonathanlurie <lurie.jo@gmail.com>
2025-09-23 22:14:12 +0200

    adding monochrome style

--- src/lib/basemapkit.ts
+++ src/lib/basemapkit.ts
@@ -11,12 +11,14 @@ import avenueLayersRaw from "./assets/avenue-layers-raw.txt?raw";
 import bureauLayersRaw from "./assets/bureau-layers-raw.txt?raw";
 import journalLayersRaw from "./assets/journal-layers-raw.txt?raw";
 import spectreLayersRaw from "./assets/spectre-layers-raw.txt?raw";
+import monochromeLayersRaw from "./assets/monochrome-layers-raw.txt?raw";
 import { getDefaultLanguage, isLanguageSupported } from "./language";
 
 const baseStyles = {
   avenue: avenueLayersRaw,
   bureau: bureauLayersRaw,
   journal: journalLayersRaw,
+  monochrome: monochromeLayersRaw,
   spectre: spectreLayersRaw,
 } as const;
 
